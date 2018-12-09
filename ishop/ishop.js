a561ad18b7ed190eb9ea87e764ce06aea13f8d60
Ellina <ellinasereda18@gmail.com>
2018-12-09 18:18:01 +0300

    work

--- ishop/ishop.js
+++ ishop/ishop.js
@@ -11,7 +11,7 @@ var IShop = React.createClass({
       for ( var a=0; a<this.props.goods.length; a++ ) {
         var product=this.props.goods[a];
         var productCode=        
-          React.DOM.tr({key:good.code,className:'Product'},
+          React.DOM.tr({key:product.code,className:'Product'},
             React.DOM.td({className:'Name'},product.name),
             React.DOM.td({className:'IMG'},
                 React.DOM.img({className:'IMG',src:product.imgURL}),
@@ -21,10 +21,20 @@ var IShop = React.createClass({
           );
         productsCode.push(productCode);
       }
-      return React.DOM.table( {className:'VotesBlock'}, 
-
-        React.DOM.div( {className:'Question'}, this.props.question ),
-        React.DOM.div( {className:'Answers'}, answersCode ),
+      return React.DOM.div( {className:'IShop'}, 
+        React.DOM.h2( {className:'ShopName'},this.props.shopName),
+        React.DOM.table( {className:'Table'},
+            React.DOM.thead( {className:'Head'},
+                React.DOM.tr({className:'HeadTr'},
+                    React.DOM.th({className:'HeadName'},'Name'),
+                    React.DOM.th({className:'HeadIMG'},'Picture'),
+                    React.DOM.th({className:'HeadPrice'},'Price'),
+                    React.DOM.th({className:'HeadRemain'},'Remain'),
+                ),
+            ),
+            React.DOM.tbody( {className:'Products'}, productsCode ), 
+                
+            ),
       );
     },
   
