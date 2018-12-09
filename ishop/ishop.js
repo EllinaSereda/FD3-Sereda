334982bc86d8641274ad44f3fd2eb42b1420f76b
Ellina <ellinasereda18@gmail.com>
2018-12-09 19:51:58 +0300

    2

--- ishop/ishop.js
+++ ishop/ishop.js
@@ -1,26 +1,27 @@
 var IShop = React.createClass({
 
-    displayName: 'VotesBlock',
+    displayName: 'IShop',
+
+    propTypes: {     //Должен получить массив, элементами которого являются объекты (хэши) со следующими свойстваними (ключами)
+        shopName: React.PropTypes.string.isRequired, 
+        goods:React.PropTypes.arrayOf(
+          React.PropTypes.shape({
+            name: React.PropTypes.string.isRequired, //Название товара
+            code: React.PropTypes.number.isRequired, //Уникальный код
+            imgURL: React.PropTypes.string.isRequired, // URL Адрес изображения
+            price: React.PropTypes.number.isRequired, //Цена
+            count: React.PropTypes.number.isRequired, //Количество товара в наличии 
+          })
+        )
+      },
   
-    getDefaultProps: function() {
-      return { question: "Вопрос ни о чём" }
-    },
   
     render: function() {
-      var productsCode=[];
-      for ( var a=0; a<this.props.goods.length; a++ ) {
-        var product=this.props.goods[a];
-        var productCode=        
-          React.DOM.tr({key:product.code,className:'Product'},
-            React.DOM.td({className:'Name'},product.name),
-            React.DOM.td({className:'IMG'},
-                React.DOM.img({className:'IMG',src:product.imgURL}),
-            ),
-            React.DOM.td({className:'Pricr'},product.price),
-            React.DOM.td({className:'Count'},product.count),
-          );
-        productsCode.push(productCode);
-      }
+      var productsCode=this.props.goods.map( v =>
+        React.createElement(Products, { key:v.code,
+          name:v.name, code:v.code, price:v.price, imgURL:v.imgURL,count:v.count} )
+      );
+
       return React.DOM.div( {className:'IShop'}, 
         React.DOM.h2( {className:'ShopName'},this.props.shopName),
         React.DOM.table( {className:'Table'},
@@ -32,9 +33,8 @@ var IShop = React.createClass({
                     React.DOM.th({className:'HeadRemain'},'Remain'),
                 ),
             ),
-            React.DOM.tbody( {className:'Products'}, productsCode ), 
-                
-            ),
+            React.createElement(Products, {goods:this.props.goodså} ),     
+          ),
       );
     },
   
