80a68920d679102336ac747506749bb013811486
Ellina <ellinasereda18@gmail.com>
2019-01-09 11:46:18 +0300

    Ready

---
+++ RainbowFrame/Components/RainbowFrame.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './RainbowFrame.css';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    colors: PropTypes.array.isRequired,
+  };
+  
+  render() {
+    console.log(this.props.colors);
+    let code=null;
+    this.props.colors.forEach( (v,i,a) => 
+        {i==0? code=<div style={{border:"5px solid "+v,padding:"10px"}}>{this.props.children}</div>:
+        code=<div style={{border:"5px solid "+v,padding:"10px"}}>{code}</div>
+    })
+    return (
+      <div className="RainbowFrame">
+        {code}
+      </div>
+    );
+  }
+
+}
+
+export default RainbowFrame;
